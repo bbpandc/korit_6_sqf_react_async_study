abb137d5eb4393ecc198b700f8109a100e1202b7
bbpandc <haeminsia@gmail.com>
2024-07-23 17:02:45 +0900

    product, Promise

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import PostPage from './pages/basic/PostPage';
 import MainLayout from './components/MainLayout/MainLayout';
 import Sidebar from './components/Sidebar/Sidebar';
 import MainContainer from './components/MainContainer/MainContainer';
+import PostPage2 from './pages/basic/PostPage2';
+import PromisePage from './pages/basic/PromisePage';
 
 
 
@@ -25,6 +27,8 @@ function App() {
             <Route path="/async/basic/get" element={<GetPage />}/>
             <Route path="/async/basic/put" element={<PutPage />}/>
             <Route path="/async/basic/delete" element={<DeletePage />}/>
+            <Route path="/async/basic/post2" element={<PostPage2 />}/>
+            <Route path="/async/basic/promise" element={<PromisePage />}/>
           </Routes>
         </MainContainer>
       </MainLayout>
